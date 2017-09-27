cc0657814a1fe6d991d1a39b233d78b03ad23ec1
Hawkon Gustavsson <h.gustavsson96@gmail.com>
2017-09-27 17:44:18 -0400

    Near-final submission. Double checking before handing in.

--- src/responses.js
+++ src/responses.js
@@ -7,74 +7,87 @@ let etag = crypto.createHash('sha1').update(JSON.stringify(users));
 let digest = etag.digest('hex');
 
 const respondJSON = (request, response, status, obj) => {
-    const headers = {
-        'Content-Type': 'application/json',
-        etag: digest,
-    };
-    response.writeHead(status, headers);
-    response.write(JSON.stringify(obj))
-    response.end();
+  const headers = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(obj));
+  response.end();
 };
 
 // Sends only the headers back
 // Could be part of respondJSON, but this way doesn't
 // require an object to be generated in the first place
 const respondMeta = (request, response, status) => {
-    const headers = {
-        'Content-Type': 'application/json',
-        etag: digest,
-    };
-    response.writeHead(status, headers);
-    response.end();
+  const headers = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+  response.writeHead(status, headers);
+  response.end();
 };
 
 // Check if we should respond w/ metadata before making
 // object - otherwise, make object and send back normally
 
 const getUsers = (request, response) => {
-    // Is the client up-to-date?
-    if(request.headers['if-none-match'] === digest){
-        // Yes
-        return respondMeta(request, response, 304);
-    }
-    // No
-    if(request.method === 'HEAD'){
-        return respondMeta(request, response, 200);
-    }
-    // The data the client is asking for
-    const obj = {users};
-    return respondJSON(request, response, 200, obj);
+  // Is the client up-to-date?
+  if (request.headers['if-none-match'] === digest) {
+    // Yes
+    return respondMeta(request, response, 304);
+  }
+  // No
+  if (request.method === 'HEAD') {
+    return respondMeta(request, response, 200);
+  }
+  // The data the client is asking for
+  const obj = { users };
+  return respondJSON(request, response, 200, obj);
 };
 
-/// HIGH PRIORITY
+// / HIGH PRIORITY
 // Still not fleshed out!
-const addUser = (request, response) => {
-    const entry = {
+const addUser = (request, response, body) => {
+  let code = 400;
+  const res = {
+    message: 'Both name and age are required parameters.',
+  };
+  if (!body.name || !body.age) {
+    res.id = 'missingParameters';
+    return respondJSON(request, response, code, res);
+  }
+  if (users[body.name]) {
+    code = 204;
+  } else { code = 201; users[body.name] = {}; }
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
 
-    };
-    users[entry.name] = entry;
+  // Stuff changed, so make sure to gen a new etag.
+  etag = crypto.createHash('sha1').update(JSON.stringify(users));
+  digest = etag.digest('hex');
 
-    //Stuff changed, so make sure to gen a new etag.
-    etag = crypto.createHash('sha1').update(JSON.stringify(users));
-    digest = etag.digest('hex');
-
-    return respondJSON(request, response, 201, {message: 'Submission Successful.'});
-
+  if (code === 201) {
+    res.id = 'addSuccess';
+    res.message = 'User Added Successfully';
+    return respondJSON(request, response, code, res);
+  }
+  return respondMeta(request, response, code);
 };
 
 const notFound = (request, response) => {
-    if(request.method === 'HEAD'){
-        return respondMeta(request, response, 404);
-    }
-    const obj = {
-        message: 'The page you are looking for is no where to be found.',
-        id: 'notFound',
-    };
-    return respondJSON(request, response, 404, obj);
+  if (request.method === 'HEAD') {
+    return respondMeta(request, response, 404);
+  }
+  const obj = {
+    message: 'The page you are looking for is no where to be found.',
+    id: 'notFound',
+  };
+  return respondJSON(request, response, 404, obj);
 };
 
 module.exports = {
-    getUsers,
-    addUser,
-    notFound
-}
+  getUsers,
+  addUser,
+  notFound,
+};
--- src/server.js
+++ src/server.js
@@ -1,46 +1,51 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 const handler = require('./responses.js');
 const htmlHandler = require('./htmlResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-    const parsedURL = url.parse(request.url);
-    
-    if(request.method === 'GET' || request.method === 'HEAD'){
-        switch(parsedURL.pathname){
-            case '/':
-            htmlHandler.getIndex(request, response);
-            break;
-            case '/style.css':
-            htmlHandler.getStyle(request, response);
-            break;
-            case '/getUsers':
-            handler.getUsers(request, response);
-            break;
-            case '/notReal':
-            default:
-            handler.notFound(request, response);
-            break;
-        }
-    /*}else if(request.method === 'HEAD'){
-        switch(parsedURL.pathname){
-            case '/getUsers':
-            // handler.getUsersMeta(request, response);
-            break;
-            case '/notReal':
-            default:
-            // handler.notFoundMeta(request, response);
-            break;
-        }*/
-    }else if(request.method === 'POST'){
-        if(parsedURL.pathname === '/addUser'){
-            // handler.addUser(request, response);
-        }
-    }else{
+  const parsedURL = url.parse(request.url);
+
+  if (request.method === 'GET' || request.method === 'HEAD') {
+    switch (parsedURL.pathname) {
+      case '/':
+        htmlHandler.getIndex(request, response);
+        break;
+      case '/style.css':
+        htmlHandler.getStyle(request, response);
+        break;
+      case '/getUsers':
+        handler.getUsers(request, response);
+        break;
+      case '/notReal':
+      default:
         handler.notFound(request, response);
+        break;
     }
+  } else if (request.method === 'POST') {
+    if (parsedURL.pathname === '/addUser') {
+      const res = response;
+      const body = [];
+      request.on('error', (err) => {
+        console.dir(err);
+        res.statusCode = 400;
+        res.end();
+      });
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      });
+      request.on('end', () => {
+        const bodyString = Buffer.concat(body).toString();
+        const bodyParams = query.parse(bodyString);
+        handler.addUser(request, response, bodyParams);
+      });
+    }
+  } else {
+    handler.notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port);
